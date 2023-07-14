229a640559965710e0cd34a15136b33553fa8926
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-07-14 17:13:20 -0400

    add show sales report functionality

--- ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
@@ -24,65 +24,69 @@ import com.shopme.common.entity.IdBasedEntity;
 @Entity
 @Table(name = "products")
 public class Product extends IdBasedEntity {
-	
+
 	@Column(unique = true, length = 255, nullable = false)
 	private String name;
-	
+
 	@Column(unique = true, length = 255, nullable = false)
 	private String alias;
-	
+
 	@Column(length = 512, nullable = false, name = "short_description")
 	private String shortDescription;
-	
+
 	@Column(length = 4096, nullable = false, name = "full_description")
 	private String fullDescription;
-	
+
 	@Column(name = "created_time")
 	private Date createdTime;
-	
+
 	@Column(name = "updated_time")
 	private Date updatedTime;
-	
+
 	private boolean enabled;
-	
+
 	@Column(name = "in_stock")
 	private boolean inStock;
-	
+
 	private float cost;
-	
+
 	private float price;
-	
+
 	@Column(name = "discount_percent")
 	private float discountPercent;
-	
+
 	private float length;
 	private float width;
 	private float height;
 	private float weight;
-	
+
 	@Column(name = "main_image", nullable = false)
 	private String mainImage;
-		
+
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	private Category category;
 
 	@ManyToOne
-	@JoinColumn(name = "brand_id")	
+	@JoinColumn(name = "brand_id")
 	private Brand brand;
-	
+
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<ProductImage> images = new HashSet<>();
-	
+
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<ProductDetail> details = new ArrayList<>();
 
-	
+
 	public Product(Integer id) {
 		this.id = id;
 	}
 
 	public Product() {
+	}
+
+	public Product(String name) {
+		this.name = name;
 	}
 
 	public String getName() {
@@ -241,15 +245,15 @@ public class Product extends IdBasedEntity {
 	public void setImages(Set<ProductImage> images) {
 		this.images = images;
 	}
-	
+
 	public void addExtraImage(String imageName) {
 		this.images.add(new ProductImage(imageName, this));
 	}
-	
+
 	@Transient
 	public String getMainImagePath() {
 		if (id == null || mainImage == null) return "/images/image-thumbnail.png";
-		
+
 		return Constants.S3_BASE_URI +"/product-images/" + this.id + "/" + this.mainImage;
 	}
 
@@ -260,7 +264,7 @@ public class Product extends IdBasedEntity {
 	public void setDetails(List<ProductDetail> details) {
 		this.details = details;
 	}
-	
+
 	public void addDetail(String name, String value) {
 		this.details.add(new ProductDetail(name, value, this));
 	}
@@ -268,20 +272,20 @@ public class Product extends IdBasedEntity {
 	public void addDetail(Integer id, String name, String value) {
 		this.details.add(new ProductDetail(id, name, value, this));
 	}
-	
+
 	public boolean containsImageName(String imageName) {
 		Iterator<ProductImage> iterator = images.iterator();
-		
+
 		while (iterator.hasNext()) {
 			ProductImage image = iterator.next();
 			if (image.getName().equals(imageName)) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	@Transient
 	public String getShortName() {
 		if (name.length() > 70) {
@@ -289,7 +293,7 @@ public class Product extends IdBasedEntity {
 		}
 		return name;
 	}
-	
+
 	@Transient
 	public float getDiscountPrice() {
 		if (discountPercent > 0) {
